0469898599c7ad6a1e8040fcdf165dba2dd82c08
Patrick Hilgenstock <pahilgenstock@hotmail.de>
2026-01-13 13:03:08 +0100

    remove old toggle logic

--- foundry-data/Data/modules/oot/scripts/main.js
+++ foundry-data/Data/modules/oot/scripts/main.js
@@ -3,40 +3,11 @@ Hooks.once('init', async function() {
 });
 
 Hooks.on('renderChatMessage', async function(_message, html) {
-
-  // Find the card content that should be collapsed
-  const cardContent = html.find('.card-content, .dnd5e2.chat-card .card-content');
-
-  if (cardContent.length > 0) {
-    // Add collapsed class by default
-    cardContent.addClass('collapsed-description');
-
-    // Find or create toggle button
-    let toggleButton = html.find('.description-toggle');
-
-    if (toggleButton.length === 0) {
-      // Create a toggle button if it doesn't exist
-      const header = html.find('.card-header, .dnd5e2.chat-card header');
-
-      if (header.length > 0) {
-        toggleButton = $('<div class="description-toggle">▶ Show Details</div>');
-        header.after(toggleButton);
-      }
-    }
-
-    // Add click handler to toggle
-    toggleButton.on('click', function(event) {
-      event.preventDefault();
-      event.stopPropagation();
-
-      const content = $(this).siblings('.card-content');
-      content.toggleClass('expanded');
-
-      if (content.hasClass('expanded')) {
-        $(this).html('▼ Hide Details');
-      } else {
-        $(this).html('▶ Show Details');
-      }
-    });
+  // Find the existing dnd5e chevron toggle button
+  const toggleButton = html.find('.fa-chevron-down');
+  console.warn('oot')
+  if (toggleButton.length > 0) {
+    // Click the toggle to collapse the content by default
+    toggleButton.click();
   }
 });
